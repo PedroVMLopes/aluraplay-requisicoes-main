167cfe2101563104d2d26b44994d9eb308bb708f
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-07-17 20:17:02 -0300

    Interações de criação de vídeos novos na plataforma integradas com o banco de dados

--- js/ApiConnect.js
+++ js/ApiConnect.js
@@ -8,6 +8,26 @@ async function videoList() {
   return convertedConnection;
 }
 
-export const apiConnect = {
-	videoList
+// Function that creates a new video in the db.json
+async function createVideo(titulo, descricao, url, imagem) {
+  const connection = await fetch("http://localhost:3000/videos", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    }),
+  });
+
+  const convertedConnection = await connection.json();
+  return convertedConnection;
 }
+
+export const apiConnect = {
+  videoList,
+  createVideo,
+};
---
+++ js/createVideo.js
@@ -0,0 +1,19 @@
+import { apiConnect } from "./ApiConnect.js";
+
+const form = document.querySelector("[data-formulario]");
+
+// Function that get the information from the form
+// and send to the function that creates it in the db.json
+async function createVideo(event) {
+  event.preventDefault();
+  const imagem = document.querySelector("[data-imagem]").value;
+  const url = document.querySelector("[data-url]").value;
+  const titulo = document.querySelector("[data-titulo]").value;
+  const descricao = Math.floor(Math.random() * 10).toString();
+
+  await apiConnect.createVideo(titulo, descricao, url, imagem);
+
+  window.location.href = "../pages/envio-concluido.html";
+}
+
+form.addEventListener("submit", (event) => createVideo(event));
